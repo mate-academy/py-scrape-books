91ed85d51d20c3db07a96eb159fdb8fae02155db
Vasyl Smutok <23.smutok@gmail.com>
2022-09-28 00:55:53 +0300

    solution parse books in single page

--- scrape_books/spiders/books.py
+++ scrape_books/spiders/books.py
@@ -1,6 +1,7 @@
+from urllib.parse import urljoin
+
 import scrapy
 from scrapy.http import Response
-from urllib.parse import urljoin
 
 
 class BooksSpider(scrapy.Spider):
@@ -8,26 +9,25 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ['books.toscrape.com']
     start_urls = ['https://books.toscrape.com/']
 
-    # @staticmethod
-    # def get_urls_for_all_books_in_page(response: Response):
-    #     return [url.css(".image_container > a ::attr(href)").get() for url in response.css(".col-xs-6")]
+    @staticmethod
+    def get_urls_for_all_books_in_page(response: Response):
+        return [url.css(".image_container > a ::attr(href)").get() for url in response.css(".col-xs-6")]
 
     def parse(self, response: Response, **kwargs):
-        # all_urls = self.get_urls_for_all_books_in_page(response)
+        all_urls = self.get_urls_for_all_books_in_page(response)
 
-        for book in response.css(".col-xs-6"):
-            yield {
-                "title": book.css(".product_pod > h3 > a::attr(title)").get(),
-                "price": book.css(".price_color::text").get(),
-                "url": book.css(".image_container > a ::attr(href)").get()
-            }
+        yield {
+            "title": response.css(".product_pod > h3 > a::attr(title)").get(),
+            "price": response.css(".price_color::text").get(),
+            # "amount_in_stock": response.css(".instock::text").get(),
+            # "rating": "1",
+            # "category": "2",
+            # "description": "3",
+            # "upc": 4,
+        }
 
-        # for url in all_urls:
-        #     book_url = urljoin(self.start_urls[0], url)
-        #     yield {
-        #         "title": response.css("div.col-sm-6.product_main > h1::text").get(),
-        #         # "price": book.css(".price_color::text").get(),
-        #         # "url": book.css(".image_container > a ::attr(href)").get()
-        #     }
-        #     yield scrapy.Request(book_url, callback=self.parse)
+        for url in all_urls:
+            book_url = urljoin(self.start_urls[0], url)
+
+            yield scrapy.Request(book_url, callback=self.parse)
 
