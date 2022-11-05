bb62b487beab94408d9bacb4c29b7915900bf35e
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-11-05 16:39:15 +0200

    Solution

--- scrapy_books/spiders/spider_books.py
+++ scrapy_books/spiders/spider_books.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import scrapy
+import re
 
 
 @dataclass
@@ -14,30 +15,58 @@ class Book:
     upc: str
 
 
+RATING = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+}
+
+
 class BookSpider(scrapy.Spider):
     name = "books-scrapy"
     start_urls = [
         "https://books.toscrape.com/"
     ]
 
-    def parse(self, response, **kwargs):
+    def parse(
+            self,
+            response: scrapy.http.Response,
+            **kwargs
+    ) -> scrapy.Request:
         for book in response.css(".product_pod"):
             detail_url = book.css("a::attr(href)").extract_first()
             yield scrapy.Request(
                 response.urljoin(detail_url),
                 self.parse_detail
             )
+        next_link = response.css(".next a::attr(href)").get()
+        yield scrapy.Request(
+            response.urljoin(next_link),
+            self.parse
+        )
 
     @staticmethod
-    def parse_detail(response, **kwargs):
+    def parse_detail(response: scrapy.http.Response) -> Book:
         yield Book(
             title=response.css(".product_main h1::text").extract_first(),
-            price=float(response.css(".price_color::text").extract_first()[1:]),
-            amount_in_stock=int("123"),
-            #     response.css(".instock::text").extract_first()[11:12]
-            # ),
-            rating=response.css(".star-rating::attr(class)").get().split()[1],
-            category="123",
-            description=response.css("#product_description + p::text").get(),
+            price=float(
+                response.css(".price_color::text").extract_first()[1:]
+            ),
+            amount_in_stock=int(
+                re.findall(
+                    r"\d+", response.css(
+                        ".availability::text"
+                    ).extract()[1].strip()
+                )[0]
+            ),
+            rating=RATING[
+                response.css(".star-rating::attr(class)").get().split()[1]
+            ],
+            category=response.css(".breadcrumb li a::text").extract()[-1],
+            description=response.css(
+                "#product_description + p::text"
+            ).get().encode("ascii", "ignore").decode(),
             upc=response.css(".table td::text").extract_first()
         )
