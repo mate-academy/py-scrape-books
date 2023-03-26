cf074e9514ce60f09a4549d656d782b1c581251a
Volodymyr Panov <vpanov25@gmail.com>
2023-03-26 13:45:21 +0200

    Solution

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy.http import Response
-from selenium import webdriver
 
 
 class BooksSpider(scrapy.Spider):
@@ -8,34 +7,33 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.driver = webdriver.Chrome()
-
-    def close(self, reason):
-        self.driver.close()
-
     def parse(self, response: Response, **kwargs):
-        all_page_books = response.css(".image_container > a::attr(href)").getall()
+        all_page_books = response.css(
+            ".image_container > a::attr(href)"
+        ).getall()
         for book in all_page_books:
             detail_page = response.urljoin(book)
-            self.driver.get(detail_page)
-            self.driver.implicitly_wait(0.5)
-            yield self.get_books_detail_info(Response(url=detail_page))
+            yield scrapy.Request(detail_page, callback=self.get_books_detail_info)
 
-        # next_page = response.css(".next > a::attr(href)").get()
-        # if next_page is not None:
-        #     next_url = response.urljoin(next_page)
-        #     yield scrapy.Request(next_url, callback=self.parse)
+        next_page = response.css(".next > a::attr(href)").get()
+        if next_page is not None:
+            next_url = response.urljoin(next_page)
+            yield scrapy.Request(next_url, callback=self.parse)
 
     @staticmethod
     def get_books_detail_info(response: Response):
         yield {
             "title": response.css("h1::text").get(),
-            "price": float(response.css(".price_color::text").get().replace("£", "")),
-            "amount_in_stock": int(response.css(".availability").get().split()[7].replace("(", "")),
-            "rating": response.css(".star-rating").get().split()[2].replace('">', ""),
-            "category": response.css(".breadcrumb > li > a").getall()[-1].split(">")[-2].replace("</a", ""),
+            "price": float(
+                response.css(".price_color::text").get().replace("£", "")
+            ),
+            "amount_in_stock": int(
+                response.css(".availability").get().split()[7].replace("(", "")
+            ),
+            "rating": response.css(
+                ".star-rating").get().split()[2].replace('">', ""),
+            "category": response.css(
+                ".breadcrumb > li > a").getall()[-1].split(">")[-2].replace("</a", ""),
             "description": response.css(".sub-header + p::text").get(),
             "UPC": response.css(".table > tr > td::text").get(),
            }
