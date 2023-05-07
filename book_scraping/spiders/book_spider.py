3345d6e22b869d139ac472fa697b729bdc2e30b9
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-05-08 00:29:13 +0300

    Refactored according PEP8

--- book_scraping/spiders/book_spider.py
+++ book_scraping/spiders/book_spider.py
@@ -2,39 +2,45 @@ import scrapy
 
 
 class BookSpider(scrapy.Spider):
-    name = 'book'
-    allowed_domains = ['books.toscrape.com']
-    start_urls = ['https://books.toscrape.com/catalogue/page-1.html']
+    name = "book"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/catalogue/page-1.html"]
 
     def parse(self, response, **kwargs):
-        for product in response.css('article.product_pod'):
-            # Extract link to product detail page
-            product_link = product.css('h3 > a::attr(href)').get()
-
-            # Follow link to product detail page and parse data
+        for product in response.css("article.product_pod"):
+            product_link = product.css("h3 > a::attr(href)").get()
             yield response.follow(product_link, self.parse_product)
 
-        # Follow link to next page
-        next_page_link = response.css('li.next > a::attr(href)').get()
+        next_page_link = response.css("li.next > a::attr(href)").get()
         if next_page_link is not None:
             yield response.follow(next_page_link, self.parse)
 
     def parse_product(self, response):
-        title = response.css('div.product_main > h1::text').get()
-        price = response.css('div.product_main > p.price_color::text').re_first(r'\d+')
-        amount_in_stock = response.css('div.product_main > p.availability::text').re_first(r'\d+')
-        rating = response.css('div.product_main > p.star-rating::attr(class)').re_first(r'star-rating (\w+)')
-        rating = int({'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}.get(rating))
-        category = response.css('ul.breadcrumb > li:nth-child(3) > a::text').get()
-        description = response.css('article.product_page > p::text').getall()
-        upc = response.css('table > tr:nth-child(1) > td::text').get()
+        title = response.css("div.product_main > h1::text").get()
+        price = response.css(
+            "div.product_main > p.price_color::text"
+        ).re_first(r"\d+")
+        amount_in_stock = response.css(
+            "div.product_main > p.availability::text"
+        ).re_first(r"\d+")
+        rating = response.css(
+            "div.product_main > p.star-rating::attr(class)"
+        ).re_first(r"star-rating (\w+)")
+        rating = int(
+            {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}.get(rating)
+        )
+        category = response.css(
+            "ul.breadcrumb > li:nth-child(3) > a::text"
+        ).get()
+        description = response.css("article.product_page > p::text").getall()
+        upc = response.css("table > tr:nth-child(1) > td::text").get()
 
         yield {
-            'title': title,
-            'price': price,
-            'amount_in_stock': amount_in_stock,
-            'rating': rating,
-            'category': category,
-            'description': description,
-            'upc': upc,
+            "title": title,
+            "price": price,
+            "amount_in_stock": amount_in_stock,
+            "rating": rating,
+            "category": category,
+            "description": description,
+            "upc": upc,
         }
