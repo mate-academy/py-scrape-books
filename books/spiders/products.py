69ac99b8f9cfc81c05d1c41a10cbeacb62079dcf
Maksym Nikityuk <maksym71417@gmail.com>
2023-05-08 16:20:26 +0300

    Solution

---
+++ books/spiders/products.py
@@ -0,0 +1,28 @@
+import scrapy
+from scrapy.http import Response
+
+
+class ProductsSpider(scrapy.Spider):
+    name = "products"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response: Response, **kwargs):
+        for book_url in response.css(".product_pod h3 a::attr(href)").getall():
+            yield response.follow(book_url, callback=self.parse_book)
+
+        next_page_url = response.css("li.next a::attr(href)").get()
+        if next_page_url:
+            yield response.follow(next_page_url, callback=self.parse)
+
+    @staticmethod
+    def parse_book(response: Response) -> dict:
+        yield {
+            "title": response.css(".product_main > h1::text").get(),
+            "price": response.css(".price_color::text").get().replace("£", ""),
+            "amount_in_stock": int(response.css(".instock.availability::text").re_first(r"\d+")),
+            "rating": response.css("p.star-rating::attr(class)").get().split()[-1],
+            "category":  response.css(".breadcrumb > li")[-2].css("a::text").get(),
+            "description": response.css("article > p::text").get(),
+            "upc": response.css(".table tr td::text").get()
+        }
