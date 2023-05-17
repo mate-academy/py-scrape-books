4fc47afb16a0c88c3a50be68b9d66f0cffaed9f5
Inna Kushnir <innakushnir63@gmail.com>
2023-05-17 13:46:16 +0300

    Solution

---
+++ scrape/spiders/books.py
@@ -0,0 +1,43 @@
+
+from typing import Generator
+import scrapy
+from scrapy.http import Response
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response: Response, **kwargs) -> Generator:
+
+        for book_url in response.css(".product_pod > h3 > a::attr(href)").extract():
+            book_url = response.urljoin(book_url)
+
+            yield scrapy.Request(url=book_url, callback=self.parse_single_product)
+
+        next_page = response.css(".next > a::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    @staticmethod
+    def parse_single_product(response: Response) -> Generator:
+        yield {
+            "title": response.css(".product_main > h1::text").get(),
+            "price": float(response.css(".product_main > .price_color::text").get().strip("£")),
+            "in_stock": int(response.css(
+                ".product_main > p.instock.availability::text"
+            ).getall()[1].strip().split()[-2].strip('(')),
+            "rating": response.css(".product_main > .star-rating").get().split()[2].strip('">'),
+            "category": response.css(".breadcrumb > li > a::text").getall()[2],
+            "description": response.css(".product_page > p::text").get(),
+            "upc": response.css(
+                ".table-striped ").get().split()[4].strip("<th>UPC</th><td>").strip("</td>"),
+        }
+
+
+if __name__ == '__main__':
+    pass
+
