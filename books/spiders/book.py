3c34ffeb562cd80b85fadac01ce1b4b905ad29be
Derkach Oleksandr <derkach03553@gmail.com>
2023-07-03 18:46:12 +0300

    Solution

---
+++ books/spiders/book.py
@@ -0,0 +1,62 @@
+from typing import Any
+from urllib.parse import urljoin
+
+import scrapy
+from selenium import webdriver
+from scrapy.http import Response
+from selenium.webdriver.common.by import By
+
+STAR_RATING_MAP = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+}
+
+
+class BookSpider(scrapy.Spider):
+    name = "book"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
+
+    def close(self, reason: Any) -> None:
+        self.driver.close()
+
+    def parse(self, response: Response, **kwargs) -> dict | Response:
+        for book in response.css(".product_pod"):
+            detailed_page_url = book.css("h3 > a::attr(href)").get()
+            amount, category, description, upc = self._get_amount_in_stock(
+                detailed_page_url, response)
+            yield {
+                "title": book.css("h3 > a::attr(title)").get(),
+                "price": float(book.css(".price_color::text").get()[1:]),
+                "amount_in_stock": amount,
+                "rating": STAR_RATING_MAP.get(
+                    book.css(".star-rating::attr(class)").get().split()[-1]),
+                "category": category,
+                "description": description,
+                "upc": upc
+            }
+
+            next_page = response.css("li.next a::attr(href)").get()
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
+
+    def _get_amount_in_stock(
+            self, url: str, response: Response) -> list[int | str]:
+        self.driver.get(urljoin(response.url, url))
+        amount_in_stock = int(self.driver.find_element(
+            By.CLASS_NAME, "instock"
+        ).text.replace("In stock (", "").split()[0])
+        category = self.driver.find_element(
+            By.CSS_SELECTOR, "ul.breadcrumb > li:nth-child(3) > a").text
+        upc = self.driver.find_element(By.TAG_NAME, "td").text
+        description_block = self.driver.find_elements(
+            By.CSS_SELECTOR, "div > article > p")
+        description = description_block[0].text if description_block else None
+        return amount_in_stock, category, description, upc
