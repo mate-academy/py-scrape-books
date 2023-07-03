b83b33ee5d746ecb9ca60ef43f59b5ef9e7307bc
Derkach Oleksandr <derkach03553@gmail.com>
2023-07-03 21:40:01 +0300

    Solution

--- books/spiders/book.py
+++ books/spiders/book.py
@@ -5,14 +5,9 @@ import scrapy
 from selenium import webdriver
 from scrapy.http import Response
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
-STAR_RATING_MAP = {
-    "One": 1,
-    "Two": 2,
-    "Three": 3,
-    "Four": 4,
-    "Five": 5
-}
+STAR_RATING_MAP = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
 
 
 class BookSpider(scrapy.Spider):
@@ -31,32 +26,56 @@ class BookSpider(scrapy.Spider):
         for book in response.css(".product_pod"):
             detailed_page_url = book.css("h3 > a::attr(href)").get()
             amount, category, description, upc = self._parse_detailed_page(
-                detailed_page_url, response)
+                detailed_page_url, response
+            )
             yield {
                 "title": book.css("h3 > a::attr(title)").get(),
                 "price": float(book.css(".price_color::text").get()[1:]),
                 "amount_in_stock": amount,
                 "rating": STAR_RATING_MAP.get(
-                    book.css(".star-rating::attr(class)").get().split()[-1]),
+                    book.css(".star-rating::attr(class)").get().split()[-1]
+                ),
                 "category": category,
                 "description": description,
-                "upc": upc
+                "upc": upc,
             }
 
             next_page = response.css("li.next a::attr(href)").get()
             if next_page is not None:
                 yield response.follow(next_page, callback=self.parse)
 
-    def _parse_detailed_page(
-            self, url: str, response: Response) -> list[int | str]:
+    def _parse_detailed_page(self, url: str, response: Response) -> list[int | str]:
         self.driver.get(urljoin(response.url, url))
-        amount_in_stock = int(self.driver.find_element(
-            By.CLASS_NAME, "instock"
-        ).text.replace("In stock (", "").split()[0])
-        category = self.driver.find_element(
-            By.CSS_SELECTOR, "ul.breadcrumb > li:nth-child(3) > a").text
-        upc = self.driver.find_element(By.TAG_NAME, "td").text
+        amount_in_stock = 0
+        category = ""
+        upc = ""
+        description = ""
+
+        try:
+            amount_in_stock = int(
+                self.driver.find_element(By.CLASS_NAME, "instock")
+                .text.replace("In stock (", "")
+                .split()[0]
+            )
+        except NoSuchElementException:
+            pass
+
+        try:
+            category = self.driver.find_element(
+                By.CSS_SELECTOR, "ul.breadcrumb > li:nth-child(3) > a"
+            ).text
+        except NoSuchElementException:
+            pass
+
+        try:
+            upc = self.driver.find_element(By.TAG_NAME, "td").text
+        except NoSuchElementException:
+            pass
+
         description_block = self.driver.find_elements(
-            By.CSS_SELECTOR, "div > article > p")
-        description = description_block[0].text if description_block else None
+            By.CSS_SELECTOR, "div > article > p"
+        )
+
+        if description_block:
+            description = description_block[0].text
         return amount_in_stock, category, description, upc
