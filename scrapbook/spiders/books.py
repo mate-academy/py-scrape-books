be6bbdf78911f2d936eed43229aeca91899f2176
Andriy <andriykrav@gmail.com>
2023-07-12 23:23:34 +0300

    solution

---
+++ scrapbook/spiders/books.py
@@ -0,0 +1,50 @@
+import json
+import scrapy
+from scrapy.http import Response
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response: Response, **kwargs):
+        for book in response.css(".product_pod"):
+            next_page_url = response.urljoin(
+                book.css(".product_pod h3 a::attr(href)").get()
+            )
+            yield response.follow(
+                next_page_url, self.parse_book_details, meta={
+                    "category_url": next_page_url
+                }
+            )
+
+        next_page_link = response.css(".next a::attr(href)").get()
+        if next_page_link:
+            yield response.follow(next_page_link, self.parse)
+
+    def parse_book_details(self, response: Response):
+        category = response.css(".breadcrumb li:nth-child(3) a::text").get()
+        availability = response.css(
+            ".instock.availability::text").re_first(r"\d+"
+        )
+        description = response.css("#product_description + p::text").get()
+        upc = response.css("tr:nth-of-type(1) td::text").get()
+
+        book_info = {
+            "title": response.css(".product_main h1::text").get(),
+            "price": response.css(".product_main .price_color::text").get(),
+            "amount_in_stock": availability.strip() if availability else None,
+            "rating": response.css(
+                ".product_main .star-rating::attr(class)"
+            ).re_first(r"star-rating\s(.+)"),
+            "category": category.strip() if category else None,
+            "description": description.strip() if description else None,
+            "upc": upc.strip() if upc else None,
+        }
+
+        with open("books.jl", "a") as f:
+            f.write(json.dumps(book_info) + "\n")
+        self.log("Saved book data to file")
+
+        yield book_info
