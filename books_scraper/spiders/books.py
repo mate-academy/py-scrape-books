e09261ec9ede27ad82b0dbc957ce956aa74e210e
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-12 13:33:22 +0300

    Create parse_single_book inside BooksSpider for parsing information from detailed url, change logic in  parse method

--- books_scraper/spiders/books.py
+++ books_scraper/spiders/books.py
@@ -8,13 +8,21 @@ class BooksSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response: Response, **kwargs) -> None:
-        for book in response.css(".col-lg-3"):
-            yield {
-                "tittle": book.css("h3 > a::attr(title)").get()
-            }
+        # "h3 a::attr(href)" == "h3 > a::attr(href)"
+        # book_urls = response.css(".product_pod > a::attr(href)").getall()
+        book_urls = response.css("h3 a::attr(href)").getall()
+
+        for book_url in book_urls:
+            yield response.follow(book_url, callback=self.parse_single_book)
 
         # next_page = response.css(".pager > li.next > a::attr(href)").get()
         next_page = response.css(".next > a::attr(href)").get()
 
         if next_page:
             yield response.follow(next_page, callback=self.parse)
+
+    @staticmethod
+    def parse_single_book(response: Response) -> None:
+        yield {
+                "title": response.css(".product_main > h1::text").get()
+            }
