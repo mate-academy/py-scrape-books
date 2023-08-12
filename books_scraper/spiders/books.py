7fb3934ca1942c6872bbc8d73a878417a76b2660
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-12 13:15:26 +0300

    Make parsing of book titles on all pages

--- books_scraper/spiders/books.py
+++ books_scraper/spiders/books.py
@@ -1,10 +1,20 @@
 import scrapy
+from scrapy.http import Response
 
 
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/catalogue/page-1.html"]
+    start_urls = ["https://books.toscrape.com/"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: Response, **kwargs) -> None:
+        for book in response.css(".col-lg-3"):
+            yield {
+                "tittle": book.css("h3 > a::attr(title)").get()
+            }
+
+        # next_page = response.css(".pager > li.next > a::attr(href)").get()
+        next_page = response.css(".next > a::attr(href)").get()
+
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
