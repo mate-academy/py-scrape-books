11a247f8d781c56d1634364470e5c567e467a88e
Diana Shyrokikh <diashiroks@gmail.com>
2023-08-30 17:36:22 +0300

    Deleted comments in items.py

--- books_to_scrape/items.py
+++ books_to_scrape/items.py
@@ -1,14 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class BooksToScrapeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     title = scrapy.Field()
     price = scrapy.Field()
     amount_in_stock = scrapy.Field()
