f0b2f4e884418e5b4db802e12813a36c55fb4ceb
Yeva <yevabrookman@gmail.com>
2023-11-13 20:04:11 +0200

    Solution

---
+++ scrape_books/spiders/books.py
@@ -0,0 +1,33 @@
+import scrapy
+
+from scrapy.http import Response
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response: Response, **kwargs):
+        for card in response.css(".product_pod"):
+            detailed_url = response.urljoin(card.css("a::attr(href)").get())
+            yield scrapy.Request(url=detailed_url, callback=self.parse_single_book)
+
+        next_page = response.css("li.next a::attr(href)").get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    @staticmethod
+    def parse_single_book(response: Response) -> dict:
+        yield {
+            "title": response.css(".product_main > h1::text").get(),
+            "price": float(response.css(".price_color::text").get().replace("£", "")),
+            "amount_in_stock": int(
+                response.css(".instock.availability::text").re_first(r"\d+")
+            ),
+            "rating": response.css("p.star-rating::attr(class)").get().split()[1],
+            "category": response.css(".breadcrumb > li:nth-child(3) a::text").get(),
+            "description": response.css("article > p::text").get(),
+            "upc": response.css(".table tr:nth-child(1) td::text").get(),
+        }
