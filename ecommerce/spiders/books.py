729742f96c2c3980ca8effdb42adcb7c154b1792
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-11-30 11:40:28 +0200

    Implement the scrape books task

---
+++ ecommerce/spiders/books.py
@@ -0,0 +1,40 @@
+import scrapy
+from scrapy.http import Response
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+    RATINGS = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+
+    def parse(self, response: Response, **kwargs) -> dict:
+        for book in response.css(".product_pod"):
+            yield scrapy.Request(
+                url=response.urljoin(book.css("h3 > a::attr(href)").get()),
+                callback=self.get_single_book,
+            )
+
+            next_page = response.css(".next > a::attr(href)").get()
+
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
+
+    def get_single_book(self, response: Response) -> dict:
+        yield {
+            "title": response.css(".product_main > h1::text").get(),
+            "price": float(
+                response.css(".price_color::text").get().replace("£", "")
+            ),
+            "amount_in_stock": int(
+                response.css(".instock::text").re_first(r"\d+")
+            ),
+            "rating": self.RATINGS.get(
+                response.css(".star-rating::attr(class)").get().split()[-1]
+            ),
+            "category": response.css(
+                ".breadcrumb > li > a::text"
+            ).getall()[-1],
+            "description": response.css(".product_page > p::text").get(),
+            "upc": response.css(".table > tr > td::text").get(),
+        }
