2b71b3040ba996f78db2ae209d079065b639bf9a
Olena Liubynetska <44604027+olenaliuby@users.noreply.github.com>
2024-01-16 11:33:25 +0000

    fix: Change return annotation Iterator to Generator

--- books/spiders/books_info.py
+++ books/spiders/books_info.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Generator
 
 import scrapy
 
@@ -8,7 +8,7 @@ class BooksInfoSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    def parse(self, response, **kwargs) -> Iterator[scrapy.Request]:
+    def parse(self, response, **kwargs) -> Generator[scrapy.Request]:
         for book in response.css(".product_pod"):
             detail_url = response.urljoin(book.css("h3 > a::attr(href)").get())
             yield scrapy.Request(
