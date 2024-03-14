e7da8a8783a558d819707a7284265e037624eead
Maxim V <m4k51m15k4m@gmail.com>
2024-03-14 17:17:54 +0200

    solution

---
+++ books/spiders/book.py
@@ -0,0 +1,49 @@
+import scrapy
+from scrapy.http import Response, Request
+
+
+class BookSpider(scrapy.Spider):
+    name = "book"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self,
+              response: Response,
+              **kwargs: any,
+              ) -> Request | Response:
+        response_css = response.css("h3 a::attr(href)")
+        for link in response_css.extract():
+            yield Request(url=response.urljoin(link), callback=self.parse_book)
+
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    @staticmethod
+    def parse_book(response: Response, ) -> dict:
+        yield {
+            "title": response.css("h1::text").get(),
+            "price": float(
+                response.css(".price_color::text").get().replace("£", "")
+            ),
+            "amount_in_stock": int(
+                response.css("tr:contains('Availability') td::text")
+                .get()
+                .split()[2]
+                .replace("(", "")
+            ),
+            "rating": {
+                "One": 1,
+                "Two": 2,
+                "Three": 3,
+                "Four": 4,
+                "Five": 5,
+            }.get(
+                response.css(".star-rating::attr(class)").get().split()[1]
+            ),
+            "category": response.css(".breadcrumb > li > a::text").getall()[2],
+            "description": response.css(
+                "#product_description + p::text"
+            ).get(),
+            "upc": response.css("tr:contains('UPC') td::text").get(),
+        }
